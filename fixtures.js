fbb438f59ce6285d363d678f44051950f7c6ddce
Will Parker <Will.Parker@widerplan.com>
2014-07-10 10:46:36 +0100

    Updated fixtures to only be used on development environment

--- fixtures.js
+++ fixtures.js
@@ -1,62 +1,66 @@
 Meteor.startup(function () {
+  enviroment = process.env.NODE_ENV;
+  if (enviroment == 'development') {
 
-  if (Roles.getAllRoles().count() === 0) {
-    Roles.createRole('superadmin');
-    Roles.createRole('admin');
-    Roles.createRole('assessor');
-    Roles.createRole('employer');
-    Roles.createRole('learner');
-    Roles.createRole('user');
-  }
+    if (Roles.getAllRoles().count() === 0) {
+      Roles.createRole('superadmin');
+      Roles.createRole('admin');
+      Roles.createRole('assessor');
+      Roles.createRole('employer');
+      Roles.createRole('learner');
+      Roles.createRole('user');
+    }
 
-  if (Courses.find().count() === 0) {
-    Courses.insert({
-      title: 'Hairdressing',
-      type: 'NVQ',
-      level: '1',
-      status: true
-    });
+    if (Courses.find().count() === 0) {
+      Courses.insert({
+        title: 'Hairdressing',
+        type: 'NVQ',
+        level: '1',
+        status: true
+      });
 
-    Courses.insert({
-      title: 'Barbering',
-      type: 'NVQ',
-      level: '1',
-      status: false
-    });
+      Courses.insert({
+        title: 'Barbering',
+        type: 'NVQ',
+        level: '1',
+        status: false
+      });
 
-     Courses.insert({
-      title: 'Hairdressing',
-      type: 'NVQ',
-      level: '2',
-      status: true
-    });
+       Courses.insert({
+        title: 'Hairdressing',
+        type: 'NVQ',
+        level: '2',
+        status: true
+      });
 
-    Courses.insert({
-      title: 'Barbering',
-      type: 'NVQ',
-      level: '2',
-      status: false
-    });
+      Courses.insert({
+        title: 'Barbering',
+        type: 'NVQ',
+        level: '2',
+        status: false
+      });
 
-    Courses.insert({
-      title: 'Hairdressing',
-      type: 'NVQ',
-      level: '3',
-      status: true
-    });
+      Courses.insert({
+        title: 'Hairdressing',
+        type: 'NVQ',
+        level: '3',
+        status: true
+      });
 
-    Courses.insert({
-      title: 'Barbering',
-      type: 'NVQ',
-      level: '3',
-      status: false
-    });
+      Courses.insert({
+        title: 'Barbering',
+        type: 'NVQ',
+        level: '3',
+        status: false
+      });
 
-    Courses.insert({
-      title: 'Car Mechanics',
-      type: 'NVQ',
-      level: '1',
-      status: false
-    });
+      Courses.insert({
+        title: 'Car Mechanics',
+        type: 'NVQ',
+        level: '1',
+        status: false
+      });
+    }
+    
   }
 });
